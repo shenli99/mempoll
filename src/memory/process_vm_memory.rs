ae1f6f36d834446247dfcd1e7603c7ddc9be0aec
shenli99 <2797255267@qq.com>
2025-05-09 23:08:36 +0800

    achieve to read/write mem by process_vm_readv/ process_vm_writev

--- src/memory/process_vm_memory.rs
+++ src/memory/process_vm_memory.rs
@@ -1,48 +1,102 @@
-use std::fs::File;
-
+use std::{io::{IoSlice, IoSliceMut}, mem::{size_of, MaybeUninit}};
+use nix::{sys::uio::{process_vm_readv, process_vm_writev, RemoteIoVec, }, unistd::Pid};
 use crate::process::Process;
 
 use super::{MemoryError, MemoryReader, MemoryWriter};
 
 pub struct ProcessVmMemory {
     process: Process,
-    file: Option<File>
 }
 
 impl ProcessVmMemory {
     pub fn new(pid: u32) -> Self {
         ProcessVmMemory {
             process: Process::new(pid),
-            file: None
         }
-    }
-
-    pub fn open(&mut self) -> Result<(), MemoryError> {
-        if self.file.is_none() {
-            let path = format!("/proc/{}/mem", self.process.pid);
-            self.file = Some(File::open(path).map_err(|e| MemoryError::ProcMemError(e.to_string()))?)
-        }
-
-        Ok(())
     }
 }
 
 impl MemoryReader for ProcessVmMemory {
     fn read<T: Sized + Copy>(&self, address: usize) -> Result<T, MemoryError> {
-        unimplemented!()
+        let mut res = MaybeUninit::<T>::uninit();
+        let size = size_of::<T>();
+
+        let mut local_iov = [ IoSliceMut::new( unsafe {
+            std::slice::from_raw_parts_mut(res.as_mut_ptr() as *mut u8, size)
+        }) ];
+
+        let remote_iov = [ RemoteIoVec{
+            base: address,
+            len: size
+        } ];
+
+        let len = process_vm_readv(Pid::from_raw(self.process.pid as i32), local_iov.as_mut_slice(), remote_iov.as_slice())
+            .map_err(|e|MemoryError::ProcessVmReadError(e.to_string()))?;
+
+        if len != size {
+            Err(MemoryError::ProcessVmReadError("Short read".to_string()))
+        }else{
+            Ok(unsafe {
+                res.assume_init()
+            })
+        }
     }
 
     fn readbuf(&self, address: usize, buf: &mut [u8]) -> Result<usize, MemoryError> {
-        unimplemented!()
+        let size = buf.len();
+        let mut local_iov = [ IoSliceMut::new(buf) ];
+        let remote_iov = [ RemoteIoVec{
+            base: address,
+            len: size
+        }];
+
+        let len = process_vm_readv(Pid::from_raw(self.process.pid as i32), local_iov.as_mut_slice(), remote_iov.as_slice())
+            .map_err(|e|MemoryError::ProcessVmReadError(e.to_string()))?;
+
+        if size != len {
+            Err(MemoryError::ProcessVmReadError("Short read".to_string()))
+        }else{
+            Ok(len)
+        }
     }
 }
 
 impl MemoryWriter for ProcessVmMemory {
     fn write<T: Sized + Copy>(&self, address: usize, value: &T) -> Result<(), MemoryError> {
-        unimplemented!()
+        let size = size_of::<T>();
+        let local_iov = [ IoSlice::new(unsafe {
+            std::slice::from_raw_parts(value as *const T as *const u8, size)
+        }) ]; 
+        let remote_iov = [ RemoteIoVec{
+            base: address,
+            len: size
+        } ];
+
+        let len = process_vm_writev(Pid::from_raw(self.process.pid as i32), local_iov.as_slice(), remote_iov.as_slice())
+            .map_err(|e|MemoryError::ProcessVmWriteError(e.to_string()))?;
+
+        if len != size {
+            Err(MemoryError::ProcessVmWriteError("Short written".to_string()))
+        }else{
+            Ok(())
+        }
     }
 
     fn writebuf(&self, address: usize, buf: &[u8]) -> Result<usize, MemoryError> {
-        unimplemented!()
+        let size = buf.len();
+        let local_iov = [ IoSlice::new(buf)];
+        let remote_iov = [ RemoteIoVec{
+            base: address,
+            len: size
+        }];
+
+        let len = process_vm_writev(Pid::from_raw(self.process.pid as i32), local_iov.as_slice(), remote_iov.as_slice())
+            .map_err(|e|MemoryError::ProcessVmWriteError(e.to_string()))?;
+
+        if len != size {
+            Err(MemoryError::ProcessVmWriteError("Short written".to_string()))
+        }else{
+            Ok(size)
+        }
     }
 }
